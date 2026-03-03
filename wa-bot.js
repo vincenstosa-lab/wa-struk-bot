f607483a07864f1bed1cc523f54ff2f660e305f2
vincenstosa-lab <vincenstosa@gmail.com>
2026-03-03 19:53:47 +0700

    fix: 2portdefine starting variable

--- wa-bot.js
+++ wa-bot.js
@@ -105,7 +105,11 @@ app.get('/qr', async (req, res) => {
   `)
 })
 
-app.listen(process.env.PORT || 3000)
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`)
+})
 
 /* ================= GOOGLE ================= */
 
